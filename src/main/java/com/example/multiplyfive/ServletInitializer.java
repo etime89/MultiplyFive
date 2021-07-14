22ea302e5b84706d46591aca70c5b344836b707a
Amosu <emmanuel.amosu1@t-mobile.com>
2021-07-14 18:06:07 -0400

    Change build.gradle to build jar

--- src/main/java/com/example/multiplyfive/ServletInitializer.java
+++ src/main/java/com/example/multiplyfive/ServletInitializer.java
@@ -1,13 +1,13 @@
-package com.example.multiplyfive;
-
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
-public class ServletInitializer extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(MultiplyFiveApplication.class); // Replace DemoApplication with your main class
-    }
-
-}
+//package com.example.multiplyfive;
+//
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+//
+//public class ServletInitializer extends SpringBootServletInitializer {
+//
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(MultiplyFiveApplication.class); // Replace DemoApplication with your main class
+//    }
+//
+//}
